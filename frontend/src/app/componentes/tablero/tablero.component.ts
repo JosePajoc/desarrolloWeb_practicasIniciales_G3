feff75dae982ee2751a314e2804c5c7d1c088b04
José Pajoc <computacion.programacion.web@gmail.com>
2021-04-07 18:04:54 -0600

    Mostrar lista usuarios del backend en el frontend

--- frontend/src/app/componentes/tablero/tablero.component.ts
+++ frontend/src/app/componentes/tablero/tablero.component.ts
@@ -12,18 +12,14 @@ import { Usuario } from 'src/app/modeloDatos/usuarios';
 export class TableroComponent implements OnInit {
   //Agregar el nombre del usuario al html obtenido de la memoria del navegador
   nombreUsuario = sessionStorage.getItem('usuario');
-
-  usuarios: Usuario[] = [
-    {carnet: 1, nombre: 'nombre1', apellido: 'apellido1'}, 
-    {carnet: 2, nombre: 'nombre2', apellido: 'apellido2'},
-    {carnet: 3, nombre: 'nombre3', apellido: 'apellido3'}
-  ];
+  //Guarda el arreglo que da como respuesta el backend
+  usuarios;
 
   constructor(private usuarioServicios: UsuariosService) { }
 
   ngOnInit(): void {
     this.usuarioServicios.verUsuarios().subscribe(
-      res => {console.log(res), 
+      res => {console.log(res); this.usuarios=res, 
       err => console.log(err)}
       );
   }
